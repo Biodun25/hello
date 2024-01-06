ce4eb359962e5a453072c1a63784d54528ddd5df
Hangout <biodunkazeem98@gmail.com>
2024-01-06 12:14:36 -0600

    first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+print("enter two integers, and I will tell you the relationships they satisfy")
+
+number1 = int(input("enter first integer"))
+number2 = int(input("enter second integer"))
+
+if number1 == number2:
+
+    print(number1, "is equals", number2)
+
+if number1 != number2:
+
+    print(number1, "is not equals", number2)
+
+if number1 < number2:
+
+    print(number1, "is less than", number2)
+
+if number1 > number2:
+    print(number1, "is greater than", number2)
+
+
